938a7c671d65af5338d4dd3ceb87d99ac52091de
Aryan Eftekhari <aryan.eftkehari@gmail.com>
2014-05-29 20:26:10 -0700

    corrected function call

--- process.js
+++ process.js
@@ -1,36 +1,58 @@
-module.exports = function (request) {
+ module.exports = function (request) {
+    var d = new Date();
+    var base = d.getTime()
 
-	this.a = function (callback) {
 
-		callback(null, "a");
+   //var request = Request;
+
+  this.a = function (Callback) {
+    var d = new Date();
+    data = {"aTime":d.getTime()-base};
+
+      setTimeout(function () {
+        Callback(null,data );
+    }, 1000);
+
+  };
+	this.b = function (data, Callback) {
+    var d = new Date();
+    data.bTime=d.getTime()-base;
+    setTimeout(function () {
+        Callback(null, data);
+    }, 1000);
+
+	};
+
+	this.c = function (data, Callback) {
+    var d = new Date();
+    data.cTime=d.getTime()-base;
+ setTimeout(function () {
+        Callback(null, data);
+    }, 1000);
+
 
 	};
 
-	this.b = function (data, callback) {
+	this.taskA = function (Callback) {
+    var d = new Date();
+    var t = {
+      "finish_time" : d.getTime()-base
+    }
 
-		callback(null, data + "b");
-
-	};
-
-	this.c = function (data, callback) {
-
-		callback(null, data + "c");
+		Callback(null, t);
 
 	};
-
-	this.taskA = function (callback) {
-
-		callback(null, "taskA-done");
-
-	};
-	this.taskB = function (callback) {
-
-		callback(null, "taskABdone");
+	this.taskB = function (Callback) {
+  var d = new Date();
+  var t = {
+      "finish_time" : d.getTime()-base
+    }
+		Callback(null, t);
 
 	};
-	this.url_er = function (callback) {
+	this.url_er = function (Callback) {
 
-		callback(true, "URL not supported");
+		Callback(true, "URL not supported : " + request.url);
 
 	};
 };
